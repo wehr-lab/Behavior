6c537cf01392e86276c7e7917cbc0ceafa4f4f6e
NickSattler <39101714+NickSattler@users.noreply.github.com>
2019-03-20 15:51:07 -0700

    Adding

---
+++ IsAny.m
@@ -0,0 +1,8 @@
+function [proceed] = IsAny(choice,string)
+    proceed = 0;
+    for i = 1:length(choice)
+        if isequal(choice{i},{string})
+            proceed = 1;
+        end
+    end
+end
