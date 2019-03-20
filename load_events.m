0eb1483ec54bbff38ced186842f588919d20600f
NickSattler <39101714+NickSattler@users.noreply.github.com>
2019-03-20 15:55:59 -0700

    Create load_events.m

---
+++ load_events.m
@@ -0,0 +1,33 @@
+try
+    load notebook.mat
+    %check if nb and stimlog are actually there
+    if ~exist('stimlog','var')
+        stimlog=[];
+        fprintf('\nfound notebook file but there was no stimlog in it!!!');
+        fprintf('\n in principle it should be possible to reconstruct all stimuli\nfrom network events, but that would take a lot of coding, does this problem happen often enough to be worth the effort???');
+        error('found notebook file but there was no stimlog in it!!!');
+    end
+    if ~exist('nb','var')
+        nb=[];
+        warning('found notebook file but there was no nb notebook information in it!!!');
+    end
+catch
+    warning('could not find notebook file')
+end
+
+%read messages
+messagesfilename='messages.events';
+if exist(messagesfilename, 'file')~=2
+    error('Could not find messages.events file. Is this the right data directory?')
+end
+[messages] = GetNetworkEvents(messagesfilename);
+
+%read digital Events
+Eventsfilename='all_channels.events';
+[all_channels_data, all_channels_timestamps, all_channels_info] = load_open_ephys_data(Eventsfilename);
+sampleRate=all_channels_info.header.sampleRate; %in Hz
+
+%get Events and soundcard trigger timestamps 
+[Events, StartAcquisitionSec] = GetEventsAndSCT_Timestamps(messages, sampleRate, all_channels_timestamps, all_channels_data, all_channels_info, stimlog);
+%Events.message_timestamp_sec = Events.message_timestamp_sec+StartAcquisitionSec;
+%there are some general notes on the format of Events and network messages in help GetEventsAndSCT_Timestamps
