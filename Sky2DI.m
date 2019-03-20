12c2aec3a316bd92558bd93c994b2a33a2221c41
NickSattler <39101714+NickSattler@users.noreply.github.com>
2019-03-20 15:45:30 -0700

    Create Sky2DI.m

---
+++ Sky2DI.m
@@ -0,0 +1,48 @@
+function [DIframe,relation] = Sky2DI(varargin) %varargin = Skyframe,InputVid,optionalDesiredRelation
+    %Returns frame# of InputVid closest in time to input Skyframe#
+    Skyframe = varargin{1};
+    InputVid = varargin{2};
+    
+    temp = dir('Behavior_mouse*.mat');
+    load(temp.name); %load camsDIframe = 1;
+    
+    if isequal(InputVid,'Leye')
+        test = milliseconds(Leye.times - Sky.times(Skyframe));
+        frame = find(test>0,1);
+        if isempty(frame)
+            frame = length(test);
+        end
+        timeaftertime = milliseconds(Leye.times(frame)-Sky.times(Skyframe));
+        timebefortime = milliseconds(Leye.times(frame-1)-Sky.times(Skyframe));
+    elseif isequal(InputVid,'Reye')
+        test = milliseconds(Reye.times - Sky.times(Skyframe));
+        frame = find(test>0,1);
+        if isempty(frame)
+            frame = length(test);
+        end
+        timeaftertime = milliseconds(Reye.times(frame)-Sky.times(Skyframe));
+        timebefortime = milliseconds(Reye.times(frame-1)-Sky.times(Skyframe));
+    end
+    
+    if nargin == 2
+        if abs(timeaftertime)<abs(timebefortime)
+            closest = frame;
+            DIframe = (closest*2)-1; %frameA
+            relation = 'A';
+        elseif abs(timebefortime)<abs(timeaftertime)
+            closest = frame-1;
+            DIframe = (closest*2); %frameB
+            relation = 'B';
+        end
+    else
+        if isequal(varargin{3},'A')
+            closest = frame;
+            DIframe = (closest*2)-1; %frameA
+            relation = 'A';
+        elseif isequal(varargin{3},'B')
+            closest = frame-1;
+            DIframe = (closest*2); %frameB
+            relation = 'B';
+        end
+    end
+end
