8b5436e9e201207203955196185314ac428df8b4
NickSattler <39101714+NickSattler@users.noreply.github.com>
2019-03-26 13:45:42 -0700

    R4 update v_string2files

--- v_string2files.m
+++ v_string2files.m
@@ -1,7 +1,7 @@
 function [v_files] = v_string2files(v_string)
 v_files = [];
 for i = 1:length(v_string)
-    if strlength(v_string(i))>1
+    if strlength(v_string(i))>5
         v_files = [v_files,v_string(i)];
     end
 end
