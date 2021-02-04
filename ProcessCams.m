1b7f8a591f4bb020fe4769df0fb45857e9a92164
NickSattler <39101714+NickSattler@users.noreply.github.com>
2021-02-03 18:07:29 -0800

    New Horizons
    Lean and mean djmaus-tracking machines

--- ProcessCams.m
+++ ProcessCams.m
@@ -9,9 +9,12 @@ if nargin>=1
     [Lear] = GetAnalogVideo('Lear',Sky.TTtimes);
     [Rear] = GetAnalogVideo('Rear',Sky.TTtimes);
 %     [Forw] = GetAnalogVideo('Forw',Sky.TTtimes);
+%     [Leye] = GetAnalogVideo('Leye',Sky.TTtimes);
+%     [Reye] = GetAnalogVideo('Reye',Sky.TTtimes);
+%     [Eye] = GetAnalogVideo('Eye',Sky.TTtimes);
 end
 
-%% Save
+%% Save the camera structures in a .mat file named 'Behavior_mouse-IDnm_YYYY-MM-DDTHH_MM_SS.mat'
 Behavior = strcat('Behavior', Sky.vid.name(4:34));
 if nargin>=1
     save(Behavior,'Sky','Head','Lear','Rear')
@@ -20,7 +23,6 @@ else
 end
 
 end
-
 
 %%%%%%%%%%%% Functions %%%%%%%%%%%%
 function [Sky] = GetSkyVideo(varargin) %Returns a structure with video information
@@ -36,7 +38,6 @@ Sky.vid = dir('Sky_m*.mp4'); %raw video from bonsai
             end
         end
     end
-    
 Sky.csv = dir('Sky_m*.csv');
     if length(Sky.csv) > 1
         for i = 1:length(Sky.csv)
@@ -58,8 +59,8 @@ Sky.csv = dir('Sky_m*.csv');
     Sky.TTL = Sky.TTL(1:2:end,:); %trigger values
     Sky.TTs = find(~cellfun(@isempty,strfind(Sky.TTL,'True'))); %Framenumber for all triggered SkyCam frames
     
-    %%%% This snippet here identifies the first frame for each LED pulse,
-    %%%% and saves them as Sky.TTs (which stands for 'TrueTriggers'
+    %%%% This snippet here identifies the onset frame for each LED pulse,
+    %%%% and saves them as Sky.TTs (which stands for 'TrueTriggers')
     temp = ones(size(length(Sky.TTs))); temp(1) = 90;
     for i=2:length(Sky.TTs)
         temp(i)=Sky.TTs(i)-Sky.TTs(i-1); %subtract previous trigger framenumber.
@@ -72,8 +73,17 @@ Sky.csv = dir('Sky_m*.csv');
     Sky.NumberOfTrigs = length(Sky.TTs);                                    %number of triggers detected
     Sky.TTdur = time(between(Sky.TTtimes(1),Sky.TTtimes(end),'time'));      %duration of video between first and last trigger
     Sky.dur = time(between(Sky.times(1),Sky.times(end),'time'));            %duration of video
-    %We should incorporate a comparison with the number of SCTs for a
-    %sanity check here in the future
+    %%%% We should incorporate a comparison with the number of SCTs for a sanity check here in the future
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DLC tracks
+    DetectTracking = dir('Sky_m*000.csv'); %see if there is a DLC csv in the directory
+    if ~isequal(length(DetectTracking),0) %if so, add the tracked points to the camera structure
+        Sky.dlc.vid = dir('Sky_m*labeled.mp4');
+        Sky.dlc.csv = dir('Sky_m*000.csv');
+        Sky.dlc.raw = textscan(fopen(Sky.dlc.csv.name),'%q'); Sky.dlc.raw = Sky.dlc.raw{1};
+        [Sky] = readDLCOutput(Sky);
+    end
+        
 end
 function [video] = GetAnalogVideo(varargin) %Returns a structure with video information
 CamName = varargin{1}; TrueTrigTimes = varargin{2};
@@ -122,8 +132,44 @@ video.csv = dir(csvsearch); %timestamps from bonsai
     video.TTtimes = video.times(video.TTs);                                 %timestamps for each trigger
     video.TTdur = time(between(video.TTtimes(1),video.TTtimes(end),'time')); %duration of video between first and last trigger
     video.dur = time(between(video.times(1),video.times(end),'time'));      %duration of video
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DLC tracks
+    DetectTracking = strcat(CamName,'_m*000.csv'); DetectTracking = dir(DetectTracking); %see if there is a DLC csv in the directory
+    if ~isequal(length(DetectTracking),0) %if so, add the tracked points to the camera structure
+        vidsearch = strcat(CamName,'_m*labeled.mp4'); csvsearch = strcat(CamName,'_m*000.csv');
+        video.dlc.vid = dir(vidsearch);
+        video.dlc.csv = dir(csvsearch);
+        video.dlc.raw = textscan(fopen(video.dlc.csv.name),'%q'); video.dlc.raw = video.dlc.raw{1};
+        [video] = readDLCOutput(video);
+    end
+    
+end
+function [outputstructure] = readDLCOutput(inputstructure) %detects the unique points tracked and integrates them into the camera's matlab structure
+    titles = strsplit(inputstructure.dlc.raw{2,1},','); titles = titles(2:end);
+    numberofpoints = (length(titles))/3;        %number of unique points tracked
+    u = unique(titles); %the names of each unique point tracked
+    
+    %This snippet identifies the X,Y,&P values in the csv for each unique tracked point in the csv file
+    for i = 1:numberofpoints
+        columnIdx = [];
+        for ii = 1:length(titles)
+            columnIdx = [columnIdx;isequal(u(i),{titles{ii}})];
+        end
+        columns{1,i} = u(i);
+        columns{2,i} = find(columnIdx);
+    end
+    numberofframes = length(inputstructure.dlc.raw)-3;
+    for i = 1:numberofpoints
+        name = string(columns{1,i});
+        inputstructure.dlc.(char(name)) = dlmread(inputstructure.dlc.csv.name,',',[3,columns{2,i}(1),numberofframes+2,columns{2,i}(end)]);
+    end
+    %End of snippet
+    
+    inputstructure.numberofpoints = numberofpoints;
+    outputstructure = inputstructure;
 end
 function [Skyframe] = DI2Sky(varargin) %varargin = InputVid,DIframe
+
     %Returns Skyframe# closest in time to InputVidFrame#
     InputVid = varargin{1};
     DIframe = varargin{2};
---
+++ v_files4DLC.m
@@ -0,0 +1,114 @@
+function [v_string,v_files] = v_files4DLC(varargin) %Generates a string of specified videos for input into DLC from folders of your choosing
+%%%%%   varargin1 = the camera files you want to select ex)'Sky', 'Leye', or 'Reye'
+%%%%%   varargin2 = (optional) a predetermined list of folders that you want to generate a v_string from, to input into DLC
+
+choice = varargin{1};
+if nargin == 2
+    SelectedFolders = varargin{2};
+else
+    SelectedFolders = uigetfile_n_dir(pwd);
+    SelectedFolders = SelectedFolders';
+end
+k = 0;
+
+%% %%%%%%%%%%%%%%%% Find specific videos %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if isequal(choice,'Sky')
+    for i = 1:length(SelectedFolders)
+        SkyFiles{i,1} = dir(fullfile(SelectedFolders{i}, '**', 'Sky_m*.mp4'));
+        for ii = 1:length(SkyFiles{i,1})
+            if length(SkyFiles{i,1}(ii).name) == 38
+                if ii > 1
+                    k = k + 1;
+                    v_list{i+k,1} = strcat(SkyFiles{i,1}(ii).folder,'\',SkyFiles{i,1}(ii).name);
+                else
+                    v_list{i+k,1} = strcat(SkyFiles{i,1}(ii).folder,'\',SkyFiles{i,1}(ii).name);
+                end
+            end
+        end
+    end
+elseif isequal(choice,'Leye')
+    for i = 1:length(SelectedFolders)
+        LeyeFiles{i,1} = dir(fullfile(SelectedFolders{i}, '**', 'Leye_m*.mp4'));
+        for ii = 1:length(LeyeFiles{i,1})
+            if length(LeyeFiles{i,1}(ii).name) == 39
+                v_list{i,1} = strcat(LeyeFiles{i,1}(ii).folder,'\',LeyeFiles{i,1}(ii).name);
+            end
+        end
+    end
+elseif isequal(choice,'Reye')
+    for i = 1:length(SelectedFolders)
+        ReyeFiles{i,1} = dir(fullfile(SelectedFolders{i}, '**', 'Reye_m*.mp4'));
+        for ii = 1:length(ReyeFiles{i,1})
+            if length(ReyeFiles{i,1}(ii).name) == 39
+                v_list{i,1} = strcat(ReyeFiles{i,1}(ii).folder,'\',ReyeFiles{i,1}(ii).name);
+            end
+        end
+    end
+elseif isequal(choice,'Lear')
+    for i = 1:length(SelectedFolders)
+        LearFiles{i,1} = dir(fullfile(SelectedFolders{i}, '**', 'Lear_m*.mp4'));
+        for ii = 1:length(LearFiles{i,1})
+            if length(LearFiles{i,1}(ii).name) == 39
+                v_list{i,1} = strcat(LearFiles{i,1}(ii).folder,'\',LearFiles{i,1}(ii).name);
+            end
+        end
+    end
+elseif isequal(choice,'Rear')
+    for i = 1:length(SelectedFolders)
+        RearFiles{i,1} = dir(fullfile(SelectedFolders{i}, '**', 'Rear_m*.mp4'));
+        for ii = 1:length(RearFiles{i,1})
+            if length(RearFiles{i,1}(ii).name) == 39
+                v_list{i,1} = strcat(RearFiles{i,1}(ii).folder,'\',RearFiles{i,1}(ii).name);
+            end
+        end
+    end
+elseif isequal(choice,'Eye')
+    for i = 1:length(SelectedFolders)
+        EyeFiles{i,1} = dir(fullfile(SelectedFolders{i}, '**', 'Eye_m*.mkv'));
+        for ii = 1:length(EyeFiles{i,1})
+            if length(EyeFiles{i,1}(ii).name) == 38
+                v_list{i,1} = strcat(EyeFiles{i,1}(ii).folder,'\',EyeFiles{i,1}(ii).name);
+            end
+        end
+    end
+elseif isequal(choice,'Head')
+    for i = 1:length(SelectedFolders)
+        HeadFiles{i,1} = dir(fullfile(SelectedFolders{i}, '**', 'Head_m*.mp4'));
+        for ii = 1:length(HeadFiles{i,1})
+            if length(HeadFiles{i,1}(ii).name) == 38
+                v_list{i,1} = strcat(HeadFiles{i,1}(ii).folder,'\',HeadFiles{i,1}(ii).name);
+            end
+        end
+    end
+elseif isequal(choice,'Forw')
+    for i = 1:length(SelectedFolders)
+        ForwFiles{i,1} = dir(fullfile(SelectedFolders{i}, '**', 'Forw_m*.mp4'));
+        for ii = 1:length(ForwFiles{i,1})
+            if length(ForwFiles{i,1}(ii).name) == 38
+                v_list{i,1} = strcat(ForwFiles{i,1}(ii).folder,'\',ForwFiles{i,1}(ii).name);
+            end
+        end
+    end
+end
+
+%% %%%%%%%%%%%%%%%% reformat '\' to '\\' %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+for i = 1:length(v_list) %for all videos inputted,
+    v_files{i,1} = strrep(v_list{i},'\','\\');
+end
+
+%% %%%%%%%%%%%%%%%% Generate a  string of all v_files in a python-friendly string %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[v_string] = v_files2string(v_files);
+end
+
+%%%%%%%%%%%%%%%% functions %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [v_string] = v_files2string(v_files) %Returns string of all v_files, to feed into python.
+
+v_string = [];
+for i = 1:length(v_files)
+    if i == 1
+        v_string = string(v_files{i});
+    else
+        v_string = [v_string,'","',string(v_files{i})];
+    end
+end
+end
