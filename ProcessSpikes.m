67cdbcb4974e5ed2bd2da732b667eabfd5b06460
mikewehr <wehr@uoregon.edu>
2025-07-25 18:09:21 -0700

    Update ProcessSpikes.m

--- ProcessSpikes.m
+++ ProcessSpikes.m
@@ -1,6 +1,6 @@
 function [SortedUnitsFile] = ProcessSpikes(varargin)
 %load kilosorted spiking data and put into SortedUnits structure and write to SortedUnits.mat file
-%usage: 
+%usage:
 % [SortedUnitsFile] = ProcessSpikes(EphysPath,LocalDataRoot) (works for older data format with earlier versions of open ephys and kilosort
 % [SortedUnitsFile] = ProcessSpikes(BonsaiPath, EphysPath_KS, EphysPath, LocalDataRoot) (new data format with new versions of open ephys and kilosort
 %
@@ -55,62 +55,95 @@ switch nargin
 end
 
 
+if exist(fullfile(BonsaiPath, 'spike_times.csv'))==2
+    %batch sorted with kilosort4, spiketimes saved in Molly's local session csv
+    T=readtable(fullfile(BonsaiPath, 'spike_times.csv'));
+    %to-do: because the filename is generic, sanity check that the filename entries match the current Bonsai folder
+    
+    [~,BonsaiFolder,~]=fileparts(BonsaiPath); %BonsaiFolder is just the timestamp-mouseID identifier of the Bonsai directory (excluding the abolute path)
+    OEinfofilename=sprintf('OEinfo-%s.mat', BonsaiFolder);
+    sampleRate = load(['../', OEinfofilename], 'OEsamplerate');
+    sampleRate=sampleRate.OEsamplerate;
+
+    cids=unique(T.cluster_id);
+    numcells=length(cids);
+    i=0;
+    for j=1:numcells;
+        group=unique(T.group(T.cluster_id==cids(j)));
+        switch group{:} %exclude 'noise' cells
+            case 'noise'
+                %do nothing
+            case {'good', 'mua'}
+                i=i+1;
+                SortedUnits(i).cellnum = i;
+                SortedUnits(i).channel = unique(T.ch(T.cluster_id==cids(j)));
+                SortedUnits(i).cluster = cids(j);
+                SortedUnits(i).spiketimes=T.time_within_file(T.cluster_id==cids(j));
+                SortedUnits(i).spiketimes=SortedUnits(i).spiketimes/sampleRate; %remove if Molly changes to sec
+                SortedUnits(i).spiketimes=SortedUnits(i).spiketimes(:)'; %enforce spiketimes is a row vector
+                SortedUnits(i).rating=group{:};
+            otherwise
+                error('unrecognized cell quality rating')
+        end %case
+    end
+else
 
 
+    sp = loadKSdir(MasterDir);
+    %LoadKSdir is from the "spikes" repository https://github.com/cortex-lab/spikes
+    % which in turn requires the npy-matlab repository https://github.com/kwikteam/npy-matlab
 
-sp = loadKSdir(MasterDir); 
-%LoadKSdir is from the "spikes" repository https://github.com/cortex-lab/spikes
-% which in turn requires the npy-matlab repository https://github.com/kwikteam/npy-matlab
+    %These two lines adapted from: https://groups.google.com/forum/?utm_medium=email&utm_source=footer#!msg/phy-users/Ydu1etOXwF0/-vEM9Rx_BgAJ
+    tempChannelAmps = squeeze(max(sp.temps,[],2))-squeeze(min(sp.temps,[],2)); % amplitude of each template on each channel, size nTemplates x nChannels
+    [~,maxChannel] = max(tempChannelAmps,[],2); % index of the largest amplitude channel for each template, size nTemplates x 1
 
-%These two lines adapted from: https://groups.google.com/forum/?utm_medium=email&utm_source=footer#!msg/phy-users/Ydu1etOXwF0/-vEM9Rx_BgAJ
-tempChannelAmps = squeeze(max(sp.temps,[],2))-squeeze(min(sp.temps,[],2)); % amplitude of each template on each channel, size nTemplates x nChannels
-[~,maxChannel] = max(tempChannelAmps,[],2); % index of the largest amplitude channel for each template, size nTemplates x 1
-
-%sp.cids are the kilosort cell IDs which you didn't label as noise. Found in 'cluster_groups.csv'
-for i=1:length(sp.cids) %each iteration here therefore corresponds to the 'cellnum' used in readKiloSortOutput.
-    if ~isequal(sp.cgs(i),3) %if the cell has been sorted, proceed to read out its information
-        template = []; Utemplate = []; numUtemplate = [];
-        template = sp.spikeTemplates(sp.clu==sp.cids(i)); %identifies the template for every spike in this cluster/cell_ID
-        Utemplate = unique(template); %identify all the unique templates contained in this cluster/cell_ID
-        if isempty(Utemplate)
-            error('no spikes match the cluster ids, this should not happen and could indicate an faulty kilosort run');
-        end
-        if length(Utemplate) == 1 %If the cluster contains spikes from only one template
-            template = ((template(1,1))+1); %then use this template (+1 because they are indexed from 0:nTemplates-1 and maxChannel is 1:nTemplates)
-            chan = maxChannel(template,1); %retun the channel that was identified to have the highest amplitude for this template
-        else
-            for k = 1:length(Utemplate) %If the cluster contains spikes from more than one template
-                numUtemplate(k,1) = sum(template(:) == Utemplate(k,1)); %find how many spikes in this cluster/cell_ID there are, for each template 
-                [val, idx] = max(numUtemplate); %find which template is most prevalent
-                template = (Utemplate(idx,1)+1); %use this template (+1 because they are indexed from 0 and maxChannel is indexed 1:nTemplates)
+    %sp.cids are the kilosort cell IDs which you didn't label as noise. Found in 'cluster_groups.csv'
+    for i=1:length(sp.cids) %each iteration here therefore corresponds to the 'cellnum' used in readKiloSortOutput.
+        if ~isequal(sp.cgs(i),3) %if the cell has been sorted, proceed to read out its information
+            template = []; Utemplate = []; numUtemplate = [];
+            template = sp.spikeTemplates(sp.clu==sp.cids(i)); %identifies the template for every spike in this cluster/cell_ID
+            Utemplate = unique(template); %identify all the unique templates contained in this cluster/cell_ID
+            if isempty(Utemplate)
+                error('no spikes match the cluster ids, this should not happen and could indicate an faulty kilosort run');
+            end
+            if length(Utemplate) == 1 %If the cluster contains spikes from only one template
+                template = ((template(1,1))+1); %then use this template (+1 because they are indexed from 0:nTemplates-1 and maxChannel is 1:nTemplates)
                 chan = maxChannel(template,1); %retun the channel that was identified to have the highest amplitude for this template
+            else
+                for k = 1:length(Utemplate) %If the cluster contains spikes from more than one template
+                    numUtemplate(k,1) = sum(template(:) == Utemplate(k,1)); %find how many spikes in this cluster/cell_ID there are, for each template
+                    [val, idx] = max(numUtemplate); %find which template is most prevalent
+                    template = (Utemplate(idx,1)+1); %use this template (+1 because they are indexed from 0 and maxChannel is indexed 1:nTemplates)
+                    chan = maxChannel(template,1); %retun the channel that was identified to have the highest amplitude for this template
+                end
+            end
+            chan = chan - 1;
+            %fn is normally the name of the .t file
+            %In the case of Kilosort data however, readKiloSortOutput is used in the Process_single
+            %function instead, and instead of using any .t files, it requires the
+            %'cellnum' which it uses to iterate through the non-noise clusters in 'cluster_groups.csv'
+            %So for now at least, I have fn = [channel, clust, cellnum]
+            fn= [chan,sp.cids(i),i];
+
+            channel = fn(1,1); clust = fn(1,2); cellnum = fn(1,3);
+
+            spiketimes = readKiloSortOutput2(clust, sp, currentdir_indx, MasterDir);
+
+            SortedUnits(i).cellnum = i;
+            SortedUnits(i).channel = chan;
+            SortedUnits(i).cluster = sp.cids(i);
+            SortedUnits(i).spiketimes = spiketimes;
+            if isequal(sp.cgs(i),2)
+                SortedUnits(i).rating = 'good';
+            elseif isequal(sp.cgs(i),1)
+                SortedUnits(i).rating = 'multiunit';
             end
         end
-        chan = chan - 1;
-        %fn is normally the name of the .t file
-        %In the case of Kilosort data however, readKiloSortOutput is used in the Process_single
-        %function instead, and instead of using any .t files, it requires the
-        %'cellnum' which it uses to iterate through the non-noise clusters in 'cluster_groups.csv'
-        %So for now at least, I have fn = [channel, clust, cellnum]
-        fn= [chan,sp.cids(i),i];
-
-        channel = fn(1,1); clust = fn(1,2); cellnum = fn(1,3);
-
-        spiketimes = readKiloSortOutput2(clust, sp, currentdir_indx, MasterDir);
-
-        SortedUnits(i).cellnum = i;
-        SortedUnits(i).channel = chan;
-        SortedUnits(i).cluster = sp.cids(i);
-        SortedUnits(i).spiketimes = spiketimes;
-        if isequal(sp.cgs(i),2)
-            SortedUnits(i).rating = 'good';
-        elseif isequal(sp.cgs(i),1)
-            SortedUnits(i).rating = 'multiunit';
-        end
     end
-end
 
-sampleRate = sp.sample_rate;
+    sampleRate = sp.sample_rate;
+
+end %if batch sorted with kilosort4 or not
 
 for unit = 1:length(SortedUnits)
     SortedUnits(unit).dir_indx = currentdir_indx;
@@ -126,11 +159,11 @@ switch nargin
         SortedUnitsFile = fullfile(EphysPath,savename);
         save(SortedUnitsFile, 'SortedUnits', 'sampleRate'); %Saves SortedUnits & sampleRate as 'SortedUnits.mat' in the ephys folder
     case 4 %new way
-                savename = strcat('SortedUnits_',EphysPath,'.mat');
+        savename = strcat('SortedUnits_',EphysPath,'.mat');
         SortedUnitsFile = fullfile(BonsaiPath,EphysPath,savename);
         save(SortedUnitsFile, 'SortedUnits', 'sampleRate'); %Saves SortedUnits & sampleRate as 'SortedUnits.mat' in the ephys folder
 
 end
-end
+
 
 
